8075af1edb1ac05f30590d20e670fe2b76760a2a
Amit Kumar <>
2023-09-09 13:53:05 +0530

    small change

--- main.py
+++ main.py
@@ -6,20 +6,22 @@ app = Flask(__name__)
 import pinecone
 from sentence_transformers import SentenceTransformer
 
-print('loading model')
-model = SentenceTransformer("./all-MiniLM-L6-v2")
+print("loading model")
+model = SentenceTransformer("all-MiniLM-L6-v2")
 
 
-pinecone_api_key = os.environ.get('PINECONE_API_KEY')
+pinecone_api_key = os.environ.get("PINECONE_API_KEY")
 
 pinecone.init(api_key=pinecone_api_key, environment="gcp-starter")
 index = pinecone.Index("icon-generator")
 
-@app.route('/')
-def hello():
-    return 'hello'
 
-@app.route('/ingest')
+@app.route("/")
+def hello():
+    return "hello"
+
+
+@app.route("/ingest")
 def hello_world():
     prompt = request.args.get("prompt")
     image_id = request.args.get("image_id")
@@ -27,11 +29,18 @@ def hello_world():
     print(prompt, image_id)
 
     embedding = model.encode(prompt)
-
-    resp = index.upsert(vectors=[{"id": image_id, "values": embedding}])
+    resp = index.upsert(
+        vectors=[
+            {
+                "id": image_id,
+                "values": embedding,
+                "metadata": {"model": "Icon", "id_type": "thumbnail"},
+            }
+        ]
+    )
 
     return jsonify({"respone": resp})
 
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
